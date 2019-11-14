45bb7ff3b9e29ea5254f11d78c91435c5f84230b
Mateus Avelar <mateus.avelar@msn.com>
2019-11-14 19:43:14 +0100

    just a demostration of a behavior to get a editing state from a textbox

---
+++ EditingStatePoc/MainWindowModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace EditingStatePoc
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        private bool _isEditing;
+
+        public bool IsEditing
+        {
+            get { return _isEditing; }
+            set
+            {
+                _isEditing = value;
+                NotifyPropertyChanged(nameof(IsEditing));
+            }
+        }
+
+        private string _someText;
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                NotifyPropertyChanged(nameof(SomeText));
+            }
+        }
+
+        private void NotifyPropertyChanged(string propertyName) => PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
